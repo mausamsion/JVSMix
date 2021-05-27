2bca9b29b60a89460042434e260e2805696bd63d
Mausam Jain <mj92android@gmail.com>
2021-05-28 00:35:23 +0900

    saving mapping as csv

--- scripts/create_jsut_naming.py
+++ scripts/create_jsut_naming.py
@@ -1,26 +1,29 @@
 import os
 import re
-import json
+import pandas as pd
 import argparse
 
 def main(args):
-    jsut_dir = args.jsut_dir
-    mapping = {}
+    mapping = pd.DataFrame()
     # get a list of all directories in jsut dataset root directory
-    subdirs = next(os.walk(jsut_dir))[1]
+    subdirs = next(os.walk(args.jsut_dir))[1]
     subdirs.sort()
     
     for sd in subdirs:
-        wavs = os.listdir(os.path.join(jsut_dir, sd, 'wav'))
+        wavs = os.listdir(os.path.join(args.jsut_dir, sd, 'wav'))
+        # keep first 3 characters and all numbers for audio file names
         for w in wavs:
             w_ = re.sub('[^0-9]', '', w)
-            mapping[w] = sd[:3]+w_
+            mapping = mapping.append([[w, sd[:3]+w_]])
     
-    with open('jsut_audio_naming.json', 'w') as fp:
-        json.dump(mapping, fp)
+    mapping.to_csv(os.path.join(args.md_dir, 'jsut_audio_naming.csv'), 
+                   index=None, header=None)
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--jsut_dir', type=str, required=True)
+    parser.add_argument('--jsut_dir', type=str, required=True,
+                        help='Absolute path of the JVS dataset directory')
+    parser.add_argument('--md_dir', type=str, required=True,
+                        help='Absolute path to metadata directory')
     args = parser.parse_args()
     main(args)
